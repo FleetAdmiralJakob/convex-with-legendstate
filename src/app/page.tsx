184a506b1dd985b9f348723a760dfd46a659d381
Jakob Rössner <jakob.roessner@outlook.de>
2024-09-27 21:43:52 +1000

    feat: Make it persist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,18 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useObservableSyncedQuery } from "@legendapp/state/sync-plugins/tanstack-react-query";
 import { convexQuery } from "@convex-dev/react-query";
 import { observer } from "@legendapp/state/react";
+import { ObservablePersistLocalStorage } from "@legendapp/state/persist-plugins/local-storage";
 
 const Home = observer(() => {
   const queryClient = useQueryClient();
   const state$ = useObservableSyncedQuery({
     queryClient,
     query: convexQuery(api.tasks.get, {}),
+    persist: {
+      name: "persistKey",
+      // Choose where to save it
+      plugin: ObservablePersistLocalStorage,
+    },
   });
 
   // Call get to start the sync
